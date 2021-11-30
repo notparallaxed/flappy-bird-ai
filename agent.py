e780a4448111675bfa148327ff6a6d3b0ee944d5
Vinicius Teixeira <viniciusmqueiroga@gmail.com>
2021-11-29 21:04:16 -0300

    q-learning improved

--- agent.py
+++ agent.py
@@ -24,6 +24,13 @@ def initialize_game(conn):
 parent_conn, child_conn = mp.Pipe()
 p = mp.Process(target=initialize_game, args=(child_conn,))
 
+def calculate_dist_bird_pipe(bird_pos, pipes_pos):
+    xdist = sensor.metrics['bird']['pos'][0] - sensor.metrics['pipes']['top']['pos'][0] 
+    if xdist < 0:
+        return round(math.dist(bird_pos, pipes_pos['top']['pos']) /10)
+    
+    return 0
+
 # Agent classes
 class Sensor():
     def __init__(self, parent_conn):
@@ -31,12 +38,7 @@ class Sensor():
         self.metrics = {}
 
     def get_dist_bird_pipes(self):
-            dist_bird_pipe_bot = math.dist(self.metrics['bird']['pos'], 
-                                           self.metrics['pipes']['bottom']['pos'])
-            dist_bird_pipe_top = math.dist(self.metrics['bird']['pos'], 
-                                           self.metrics['pipes']['top']['pos'])
-            
-            return round((dist_bird_pipe_bot + dist_bird_pipe_top)/2)
+        return calculate_dist_bird_pipe(self.metrics['bird']['pos'], self.metrics['pipes'])
 
     def update(self):
         self.metrics = self.conn.recv()
@@ -51,11 +53,7 @@ class Actuator():
     def dont_bump(self):
         self.conn.send({'bump': False})
 
-def calculate_dist_bird_pipe(bird_pos, pipes_pos):
-    dist_bird_pipe_bot = math.dist(bird_pos, pipes_pos['bottom']['pos'])
-    dist_bird_pipe_top = math.dist(bird_pos, pipes_pos['top']['pos'])
 
-    return round((dist_bird_pipe_bot + dist_bird_pipe_top)/2)
 
 class State():
     def __init__(self, bird_metrics, pipes_metrics):
@@ -71,7 +69,7 @@ class State():
             "DO_BUMP" : 0,
         }
 
-        self.index = (bird_metrics['pos'][1], self.dist_bird_pipes)
+        self.index = (round(bird_metrics['pos'][1]/10), self.dist_bird_pipes)
 
     def get_max_q_action(self):
         if (self.actions_q_value['DO_BUMP'] > self.actions_q_value['DONT_BUMP']):
@@ -88,17 +86,17 @@ class Score():
 
     def achive_goal(self):
         # Goal is gain more points
-        if self.points < self.sensor.metrics['score']['points']:
-            self.points = self.sensor.metrics['score']['points']
-            self.reward_value = 10000
-            return True
-        elif self.attempts < self.sensor.metrics['score']['attempts']:
+        if self.attempts < self.sensor.metrics['score']['attempts']:
             self.attempts = self.sensor.metrics['score']['attempts']
             self.points = self.sensor.metrics['score']['points']
-            self.reward_value = -1000
+            self.reward_value = -10
             return False
+        elif self.points < self.sensor.metrics['score']['points']:
+            self.points = self.sensor.metrics['score']['points']
+            self.reward_value = 10
+            return True
 
-        self.reward_value = -100
+        self.reward_value = 0
         return False
     
 
@@ -112,7 +110,7 @@ previous_state = None
 actual_state = None
 gamma = 8
 def alpha(attempts):
-    return math.pow(math.e, -(attempts/100))
+    return math.pow(math.e, -(attempts/5))
 
 
 # Run for 1st state
@@ -130,13 +128,15 @@ actuator.dont_bump()
 
 while p.is_alive():
     sensor.update()
-    
+
+    print("distx-bird-pipe:", calculate_dist_bird_pipe(sensor.metrics['bird']['pos'], sensor.metrics['pipes']))
+    print("states(n):", len(states))
+
     # Check if the actual perceived state has beeen observed before, set the actual state
-    if any(state.index == (sensor.metrics['bird']['pos'][1], 
+    if any(state.index == ((round(sensor.metrics['bird']['pos'][1]/10)), 
                             sensor.get_dist_bird_pipes()) for state in states):
-        actual_state = next(state for state in states if state.index == (sensor.metrics['bird']['pos'][1], 
+        actual_state = next(state for state in states if state.index == (round(sensor.metrics['bird']['pos'][1]/10), 
                             sensor.get_dist_bird_pipes()))
-        print('estado repetido')
     else:
         actual_state = State(sensor.metrics['bird'], sensor.metrics['pipes'])
         states.append(actual_state)
@@ -147,13 +147,11 @@ while p.is_alive():
         actual_state.q_values['DONT_BUMP'] = score.reward_value
     else:
         maxQ = max(actual_state.q_values, key=actual_state.q_values.get)
-        print(actual_state.q_values)
         a, r = previous_state.reward_history[-1]
         previous_state.q_values[a] = previous_state.q_values[a] + alpha(score.attempts)*(r + gamma*actual_state.q_values[maxQ] - previous_state.q_values[a] )
 
     #Defines which action takes based on q-value
     next_action = max(actual_state.q_values, key=actual_state.q_values.get)
-    print(next_action)
     if (next_action == 'DO_BUMP'):
         actual_state.reward_history.append(('DO_BUMP', score.reward_value))
         actuator.do_bump()
@@ -163,6 +161,4 @@ while p.is_alive():
 
     previous_state = actual_state
 
-  
-zip
 
